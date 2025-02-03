0b62bcf422fa2dec5e1be47fa3a69adeca4d13f4
Hieu Dang <hieudang@hieus-mbp.home>
2025-02-03 16:45:51 +0100

    create Json Database, created index.html for Flask templates Folder and style.css in static Folder

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import json
+
+def get_blogposts_from_database():
+    # Python Parse JSON
+    with open("database.json", "r") as fileobj:
+        data = json.load(fileobj)
+        return data
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    # add code here to fetch the job posts from a file
+    blog_posts = get_blogposts_from_database()
+    return render_template('index.html', posts = blog_posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
